44db9d449ce468ecfffc7be5b05241773bfd2839
mokhtarHamdoune <mokhtardark@gmail.com>
2023-10-22 22:46:19 +0100

    improve: add style to the todo list; name the app GODO

--- main.go
+++ main.go
@@ -17,10 +17,19 @@ func main (){
         log.Fatalf("Error loading env file %v",envErr)
     }
 
+
     tmpl, parsErr := template.ParseFS(os.DirFS("./templates"),"task.html")
     if(parsErr != nil){
         log.Fatalf("we couldn't read the content of the file %v",envErr)
     }
+    /* 
+        * Serve static file 
+        * StripPrefix as the name suggeste it will remove the /static/ prefix from the url
+        * FilServer will serve the files of assets dir as  css/ js/ ...
+        * If I get url /static/css/style.css => css/style.css and that will mache the Filserver content e.g. css/
+    */ 
+    http.Handle("/static/",http.StripPrefix("/static/",http.FileServer(http.Dir("./assets"))))
+    
     http.HandleFunc("/",func (w http.ResponseWriter, r *http.Request){
         tmpl.Execute(w,nil)
     })
