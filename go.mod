46863d0294e3b75ec94616621581e33158bf722d
mokhtarHamdoune <mokhtardark@gmail.com>
2023-10-22 16:03:08 +0100

    feat: read simple html template

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/mokhtarHamdouen/todo-app-go
 
 go 1.18
+
+require github.com/joho/godotenv v1.5.1 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import ("fmt")
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"text/template"
 
+	"github.com/joho/godotenv"
+)
+
+//TODO: add the style
 func main (){
-    fmt.Println("hello mom")
+    envErr := godotenv.Load(".env")
+    if envErr != nil {
+        log.Fatalf("Error loading env file %v",envErr)
+    }
+
+    tmpl, parsErr := template.ParseFS(os.DirFS("./templates"),"task.html")
+    if(parsErr != nil){
+        log.Fatalf("we couldn't read the content of the file %v",envErr)
+    }
+    http.HandleFunc("/",func (w http.ResponseWriter, r *http.Request){
+        tmpl.Execute(w,nil)
+    })
+
+    var port string = os.Getenv("PORT")
+    fmt.Printf("Server is running on port %s",port)
+    http.ListenAndServe(":" + port,nil)
+    
 }
